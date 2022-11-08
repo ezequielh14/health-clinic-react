d68fb4443ad1396f6130ba585de0ce6f86abd880
slavks <exeherrera96@gmail.com>
2022-11-08 14:35:16 -0300

    Added login and register function with context

---
+++ src/configs/firebase.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbtCEGj9oE9GPfPIr1ghEHqanCh0vcE80",
+  authDomain: "auth-health.firebaseapp.com",
+  projectId: "auth-health",
+  storageBucket: "auth-health.appspot.com",
+  messagingSenderId: "551759891632",
+  appId: "1:551759891632:web:a1fcf3a51f6cda2fa0bf75",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig); // initialize app
+export const db = getFirestore(); // this gets the firestore database
+
+//### REGISTER USER WITH FIREBASE AUTHENTICATION ###//
+export const registerUser = (email, password) => {
+  const auth = getAuth();
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+//### LOGIN USER WITH FIREBASE ###//
+export const loginUser = (email, password) => {
+  const auth = getAuth();
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+//### LOGOUT USER WITH FIREBASE ###//
+export const logoutUser = () => {
+  const auth = getAuth();
+  signOut(auth).then(() => {
+    alert('User signed out!');
+  }).catch((error) => {
+    alert('Something went wrong!');
+    const errorCode = error.code;
+    console.log(errorCode);
+  });
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar";
 
+import { logoutUser } from "../configs/firebase";
+
 const Home = () => {
     return (
         <>
             <Navbar />
             <div>Home</div>
+
+            <button onClick={logoutUser}
+            className="mt-6 block rounded-lg bg-red-500 px-3 py-3 font-semibold text-white hover:bg-red-400 focus:bg-blue-400">
+                Logout</button>
         </>
     );
 };
