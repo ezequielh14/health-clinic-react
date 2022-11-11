aa462e4b3527e8afb61653f11a9d1d43e38d07b1
celesdv <66212987+celesdv@users.noreply.github.com>
2022-11-11 12:51:21 -0300

    :sparkles: Feat: add turnList

--- src/components/FormTurns.jsx
+++ src/components/FormTurns.jsx
@@ -1,98 +1,129 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ListTurns from "./ListTurns";
 import { useNavigate } from "react-router-dom";
 
 const FormTurns = () => {
   const data = [
     {
+      id: 1,
       name: "Especialidad 1",
       professionals: [
         {
+          id: 1,
           name: "Doctor 1",
           turns: [
             {
-              date: "15/11/2022 9:30",
+              id: 1,
+              date: "15/11/2022",
+              hour: "9:30",
+              state: false,
+            },
+            {
+              id: 2,
+              date: "15/11/2022",
+              hour: "10:30",
               state: true,
             },
             {
-              date: "16/11/2022 9:30",
-              state: true,
-            },
-            {
-              date: "17/11/2022 9:30",
+              id: 3,
+              date: "15/11/2022",
+              hour: "11:30",
               state: true,
             },
           ],
         },
         {
+          id: 2,
           name: "Doctor 2",
           turns: [
             {
-              date: "15/11/2022 10:30",
+              id: 1,
+              date: "16/11/2022",
+              hour: "9:30",
               state: true,
             },
             {
-              date: "16/11/2022 10:30",
+              id: 2,
+              date: "16/11/2022",
+              hour: "10:30",
               state: true,
             },
             {
-              date: "17/11/2022 10:30",
-              state: true,
+              id: 3,
+              date: "16/11/2022",
+              hour: "10:30",
+              state: false,
             },
           ],
         },
       ],
     },
     {
+      id: 2,
       name: "Especialidad 2",
       professionals: [
         {
+          id: 1,
           name: "Doctor 3",
           turns: [
             {
-              date: "15/11/2022 9:30",
+              id: 1,
+              date: "17/11/2022",
+              hour: "9:30",
               state: true,
             },
             {
-              date: "16/11/2022 9:30",
+              id: 2,
+              date: "17/11/2022",
+              hour: "10:30",
               state: true,
             },
             {
-              date: "17/11/2022 9:30",
-              state: true,
+              id: 3,
+              date: "17/11/2022",
+              hour: "11:30",
+              state: false,
             },
           ],
         },
         {
+          id: 2,
           name: "Doctor 4",
           turns: [
             {
-              date: "15/11/2022 10:30",
+              id: 1,
+              date: "18/11/2022",
+              hour: "9:30",
               state: true,
             },
             {
-              date: "16/11/2022 10:30",
+              id: 2,
+              date: "18/11/2022",
+              hour: "10:30",
               state: true,
             },
             {
-              date: "17/11/2022 10:30",
+              id: 3,
+              date: "18/11/2022",
+              hour: "11:30",
               state: true,
             },
           ],
         },
         {
+          id: 3,
           name: "Doctor 5",
           turns: [
             {
-              date: "15/11/2022 10:30",
-              state: true,
+              id: 1,
+              date: "19/11/2022",
+              hour: "9:30",
+              state: false,
             },
             {
-              date: "16/11/2022 10:30",
-              state: true,
-            },
-            {
-              date: "17/11/2022 10:30",
+              id: 2,
+              date: "19/11/2022",
+              hour: "10:30",
               state: true,
             },
           ],
@@ -108,6 +139,22 @@ const FormTurns = () => {
   const [speciality, setSpeciality] = useState("");
   const [professional, setProfessional] = useState("");
 
+  useEffect(() => {
+    for (let element of data) {
+      if (element.name === speciality) {
+        setProfessionals(element.professionals);
+      }
+    }
+  }, [speciality]);
+
+  useEffect(() => {
+    for (let element of professionals) {
+      if (element.name === professional) {
+        setTurns(element.turns);
+      }
+    }
+  }, [professional]);
+
   const handleSpeciality = (event) => {
     setSpeciality(event.target.value);
   };
@@ -118,27 +165,16 @@ const FormTurns = () => {
 
   const handleSubmitSpeciality = (e) => {
     e.preventDefault();
-    for (let element of data) {
-      if (element.name === speciality) {
-        setProfessionals(element.professionals);
-      }
-    }
   };
 
   const handleSubmitProfessional = (e) => {
     e.preventDefault();
-    for (let element of professionals) {
-      if (element.name === professional) {
-        setTurns(element.turns);
-      }
-    }
-    navigate("/listTurns");
   };
 
   return (
     <>
       <div className="flex h-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
-        <div className="w-96 rounded-lg bg-white p-6 shadow dark:bg-gray-800">
+        <div className="w-96 rounded-lg bg-white p-6 shadow dark:bg-gray-800 m-3">
           <h2 className="text-center text-3xl font-semibold text-gray-700 dark:text-white">
             Request Turn
           </h2>
@@ -159,21 +195,11 @@ const FormTurns = () => {
                 Select speciality
               </option>
               {data.map((speciality) => (
-                <option value={speciality.name} key={speciality.name}>
+                <option value={speciality.name} key={speciality.id}>
                   {speciality.name}
                 </option>
               ))}
             </select>
-            <button
-              className="mt-6 block w-full rounded-lg bg-blue-500 px-3 py-3 font-semibold text-white hover:bg-blue-400 focus:bg-blue-400"
-              type="submit"
-              onClick={handleSubmitSpeciality}
-            >
-              Search
-            </button>
-          </form>
-
-          <form className="mt-4">
             <label
               className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-200"
               htmlFor="speciality"
@@ -189,21 +215,16 @@ const FormTurns = () => {
                 Select Professional
               </option>
               {professionals.map((professional) => (
-                <option value={professional.name} key={professional.name}>
+                <option value={professional.name} key={professional.id}>
                   {professional.name}
                 </option>
               ))}
             </select>
-            <button
-              className="mt-6 block w-full rounded-lg bg-blue-500 px-3 py-3 font-semibold text-white hover:bg-blue-400 focus:bg-blue-400"
-              type="submit"
-              onClick={handleSubmitProfessional}
-            >
-              Search
-            </button>
           </form>
         </div>
+        <ListTurns turns={turns} />
       </div>
+      
     </>
   );
 };
