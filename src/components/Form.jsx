a8354904491d0ff51635ae4bbb7565b0612ead6c
GUardini Philama <guardi2005@yahoo.fr>
2022-11-14 22:16:35 -0300

    :parkles::dart:form all feat is onlione:+1:

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [name, setName] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [dni, setDni] = useState("");
+  const [date , setBirthDay] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [street, setStreetAddress] = useState("");
+  const [phone, setTelefono] = useState("");
+  const [aporte, setObraSocial] = useState("");
   const [messageSent, setMessageSent] = useState("");
 
   return (
@@ -15,9 +20,14 @@ const Form = () => {
           id="formulario"
           onSubmit={(e) => {
             e.preventDefault();
-            setName(e.target.name.value);
+            setFirstName(e.target.firstname.value);
+            setLastName(e.target.lastname.value);
+            setDni(e.target.number.value);
+            setBirthDay(e.target.date.value);
+            setStreetAddress(e.target.street.value);
+            setTelefono(e.target.phone.value);
             setEmail(e.target.email.value);
-            setMessage(e.target.message.value);
+            setObraSocial(e.target.aporte.value);
             setMessageSent("¡Mensaje enviado!");
             console.log(e.target.name.value);
             console.log(e.target.email.value);
@@ -25,14 +35,60 @@ const Form = () => {
             e.target.reset();
           }}
         >
-          <label>Nombre: </label>
+          <label>Nombre/s: </label>
           <input
             type="text"
-            name="name"
-            placeholder="Ingresá tu nombre"
+            name="fistname"
+            placeholder="Ingresá tu nombre/s"
             onChange={(e) => setMessageSent("")}
             required
           />
+
+          <label>Apellido/s: </label>
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Ingresá tu apellido/s"
+            onChange={(e) => setMessageSent("")}
+            required
+          />
+
+<label>DNI: </label>
+          <input
+            type="number"
+            name="dni"
+            placeholder="Ingresá tu DNI"
+            onChange={(e) => setMessageSent("")}
+            required
+          />
+
+<label>F. Nacicimiento: </label>
+          <input
+            type="date"
+            name="date"
+            placeholder="Ingresá tu F. Nacimiento"
+            onChange={(e) => setMessageSent("")}
+            required
+          />
+          
+          <label>Domicilio: </label>
+          <input
+            type="text"
+            name="street"
+            placeholder="Ingresá tu Domicilio"
+            onChange={(e) => setMessageSent("")}
+            required
+          />
+
+<label>Teléfono: </label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Ingresá tu Teléfono"
+            onChange={(e) => setMessageSent("")}
+            required
+          />
+
           <label>Email: </label>
           <input
             type="email"
@@ -41,32 +97,40 @@ const Form = () => {
             onChange={(e) => setMessageSent("")}
             required
           />
-          <legend>Escribí acá tu mensaje:</legend>
-          <textarea
-            name="message"
-            cols="90"
-            rows="10"
-            placeholder="(Campo obligatorio)"
+          
+          <label>Obra Social: </label>
+          <input
+            type="text"
+            name="aporte"
+            placeholder="Ingresá tu Obra Social"
             onChange={(e) => setMessageSent("")}
             required
-          ></textarea>
+          />
+
           <button type="submit" className="button">
-            Enviar
+            guardar datos
           </button>
         </form>
+
       </section>
       <div className="printResults">
         <h2>{messageSent}</h2>
         <h3>Resumen de datos enviados</h3>
-        <p>Nombre: {name}</p>
+        <p>Nombre: {firstname}</p>
+        <p>Apellido: {lastname}</p>
+        <p>DNI: {dni}</p>
+        <p>F. Nacimiento: {date}</p>
+        <p>Domicilio: {street}</p>
+        <p>Teléfono: {phone}</p>
         <p>Email: {email}</p>
-        <p>Mensaje: {message}</p>
+        <p>Obra Social: {aporte}</p>
       </div>
     </div>
   );
 };
 
 export default Form;
+
 
 import 'react-dropdown/style.css';
 
